f784c527f7037887809e0011d6c125648c42cf86
RyoUdaka(宇髙凌生) <RyoUdaka()@Dirbato-1401x>
2024-04-08 12:09:24 +0900

    演習終了

---
+++ Calc/src/sample/Compare.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Compare {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        
+        System.out.println("a > b は" + (a > b));
+        System.out.println("a < b は" + (a < b));
+    }
+}
